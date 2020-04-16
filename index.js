16d26fd170d14324c4afc73fecef08a908c0aafa
biosan <alessandro@biondi.me>
2020-04-16 16:49:57 +0200

    chore: remove 'swag' dependency

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 const fs = require('fs');
 const handlebars = require('handlebars');
 const handlebarsWax = require('handlebars-wax');
-// const addressFormat = require('address-format');
 const moment = require('moment');
-const Swag = require('swag');
-
-try {
-  window.Swag.registerHelpers(handlebars);
-} catch (e) {
-  Swag.registerHelpers(handlebars);
-}
 
 handlebars.registerHelper({
   removeProtocol: url => url.replace(/.*?:\/\//g, ''),
@@ -18,7 +10,8 @@ handlebars.registerHelper({
   // formatAddress: (...args) => addressFormat(args).join(' '),
   formatAddress: (...args) => args.filter(arg => typeof arg !== 'object').join(' '),
   formatDate: date => moment(date).format('MM/YYYY'),
-  // lowercase: s => s.toLowerCas(),
+  lowercase: s => s.toLowerCase(),
+  eq: (a, b) => a === b,
 });
 
 function render(resume) {
